4be4b8b893222fd5ff91e85f0b54b57811e34041
eststar <eststar1@naver.com>
2025-12-19 00:04:37 +0900

    1219

--- src/main/java/com/mini/domain/EmergencyBoard.java
+++ src/main/java/com/mini/domain/EmergencyBoard.java
@@ -1,5 +1,23 @@
 package com.mini.domain;
 
-public class EmergencyBoard {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+
+public class EmergencyBoard {
+	@Id
+	private Long seq;
 }
