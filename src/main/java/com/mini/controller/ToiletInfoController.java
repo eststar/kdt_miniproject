4bc25ec51ff11d5fe517cc9aaa2ff9c53bde1329
eststar <eststar1@naver.com>
2025-12-24 00:03:01 +0900

    1224d

--- src/main/java/com/mini/controller/ToiletInfoController.java
+++ src/main/java/com/mini/controller/ToiletInfoController.java
@@ -1,7 +1,6 @@
 package com.mini.controller;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
